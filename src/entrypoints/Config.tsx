0e32c4511ff04a920fe99b0295a1753958d38b15
Ulises Himely <ulises@himely.tech>
2023-04-18 21:36:07 -0400

    Introduce new audio duration and hide field options

--- src/entrypoints/Config.tsx
+++ src/entrypoints/Config.tsx
@@ -1,5 +1,13 @@
 import { RenderManualFieldExtensionConfigScreenCtx } from 'datocms-plugin-sdk';
-import { Canvas, Form, TextField } from 'datocms-react-ui';
+import {
+  Canvas,
+  DropdownOption,
+  Form,
+  SelectField,
+  SelectInput,
+  SwitchField,
+  TextField,
+} from 'datocms-react-ui';
 import { useCallback, useState } from 'react';
 type PropTypes = {
   ctx: RenderManualFieldExtensionConfigScreenCtx;
@@ -7,14 +15,20 @@ type PropTypes = {
 // this is how we want to save our settings
 type Parameters = {
   videoField: number;
+  mediaType: {
+    label: 'Video' | 'Audio';
+    value: 'video' | 'audio';
+  };
+  hideField: boolean;
 };
 
 export default function PluginConfig({ ctx }: PropTypes) {
   const [formValues, setFormValues] = useState<Partial<Parameters>>(
     ctx.parameters,
   );
-  const update = useCallback(
-    (field: string, value: string) => {
+
+  const setSourceFieldName = useCallback(
+    (field: string, value: any) => {
       const newParameters = { ...formValues, [field]: value };
       setFormValues(newParameters);
       ctx.setParameters(newParameters);
@@ -25,13 +39,37 @@ export default function PluginConfig({ ctx }: PropTypes) {
   return (
     <Canvas ctx={ctx}>
       <Form>
+        <SelectField
+          name='mediaType'
+          id='mediaType'
+          label='Media Type'
+          hint='Select the media type'
+          value={formValues.mediaType}
+          selectInputProps={{
+            options: [
+              { label: 'Video', value: 'video' },
+              { label: 'Audio', value: 'audio' },
+            ],
+          }}
+          required
+          onChange={setSourceFieldName.bind(null, 'mediaType')}
+        />
         <TextField
           id='videoField'
           name='videoField'
-          label='Video field ID'
+          label='Video or audio field ID'
+          hint='The field that contains a link to a video or audio resource'
           required
           value={formValues.videoField}
-          onChange={update.bind(null, 'videoField')}
+          onChange={setSourceFieldName.bind(null, 'videoField')}
+        />
+        <SwitchField
+          name='hideField'
+          id='hideField'
+          label='Hide field?'
+          hint='Whether or not to hide the field in document'
+          value={formValues.hideField || false}
+          onChange={setSourceFieldName.bind(null, 'hideField')}
         />
       </Form>
     </Canvas>
--- src/entrypoints/Plugin.tsx
+++ src/entrypoints/Plugin.tsx
@@ -5,43 +5,54 @@ type Props = {
   ctx: RenderFieldExtensionCtx;
 };
 
+const formatTime = (time: number, ctx: RenderFieldExtensionCtx) => {
+  let duration = '';
+
+  if (!time) return;
+
+  const seconds = time % 60;
+  const minutes = ((time - seconds) / 60) % 60;
+  const hours = Math.floor(time / 3600);
+
+  const formatForTime = (n: number) =>
+    n < 10 ? `0${n.toString()}` : n.toString();
+
+  if (hours > 0) {
+    duration += `${formatForTime(hours)}:`;
+  }
+
+  duration += `${formatForTime(minutes)}:`;
+  duration += formatForTime(Math.floor(seconds));
+
+  ctx.setFieldValue(ctx.fieldPath, duration);
+};
+
 export default function ConfigScreen({ ctx }: Props) {
   return (
     <Canvas ctx={ctx}>
-      <video
-        style={{ display: 'none' }}
-        onLoadedData={(e) => {
-          const time = e.currentTarget.duration;
-          let duration = '';
-
-          if (!time) return;
-
-          const seconds = time % 60;
-          const minutes = ((time - seconds) / 60) % 60;
-          const hours = Math.floor(time / 3600);
-
-          const formatForTime = (n: number) =>
-            n < 10 ? `0${n.toString()}` : n.toString();
-
-          if (hours > 0) {
-            duration += `${formatForTime(hours)}:`;
-          }
-
-          duration += `${formatForTime(minutes)}:`;
-          duration += formatForTime(Math.floor(seconds));
-
-          ctx.setFieldValue(ctx.fieldPath, duration);
-        }}
-        src={ctx.formValues[ctx.parameters.videoField as string] as string}
-      />
-      <Form>
-        <FieldWrapper
-          label='Video Duration'
-          id={`${ctx.field.id}--field-wrapper`}
-        >
-          <TextInput value={ctx.formValues[ctx.fieldPath] as string} />
-        </FieldWrapper>
-      </Form>
+      {(ctx.parameters.mediaType as any).value === 'video' ? (
+        <video
+          style={{ display: 'none' }}
+          onLoadedData={(e) => formatTime(e.currentTarget.duration, ctx)}
+          src={ctx.formValues[ctx.parameters.videoField as string] as string}
+        />
+      ) : (
+        <audio
+          style={{ display: 'none' }}
+          onLoadedData={(e) => formatTime(e.currentTarget.duration, ctx)}
+          src={ctx.formValues[ctx.parameters.videoField as string] as string}
+        />
+      )}
+      <div style={{ display: ctx.parameters.hideField ? 'none' : 'block' }}>
+        <Form>
+          <FieldWrapper
+            label='Video Duration'
+            id={`${ctx.field.id}--field-wrapper`}
+          >
+            <TextInput value={ctx.formValues[ctx.fieldPath] as string} />
+          </FieldWrapper>
+        </Form>
+      </div>
     </Canvas>
   );
 }
